220bf5aa35dca0790bbf20509035d215814f70ff
Tijn Kersjes <tijn@divbyzero.nl>
2021-11-12 15:04:56 +0100

    Static build

---
+++ docs/_next/static/a2BubFtP75Dx2TGBZe_XL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/171-8e0ad7ff7fe8e782.js","static/css/e36e9ca00e00d53e.css","static/chunks/pages/index-b139738106dc73c5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
